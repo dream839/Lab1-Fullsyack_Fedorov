5de31467a1b841af73dffc8f2a885b9e3dc22a16
dream839 <mihailfedorov3973@gmail.com>
2026-02-23 12:52:17 +0300

    Initial project structure

--- obj/Debug/net10.0/Lab1_ISRPO_Fedorov.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab1_ISRPO_Fedorov.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1_ISRPO_Fedorov")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d36490ca503202c27ff1b61254c81ce4e7b57c07")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1_ISRPO_Fedorov")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1_ISRPO_Fedorov")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
