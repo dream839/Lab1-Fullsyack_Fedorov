2e7f5055d67603888e33a5abbe51b032e09abe40
dream839 <mihailfedorov3973@gmail.com>
2026-02-23 13:25:24 +0300

    Added basic backend program

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Меня зовут Федоров М.Д.");
+Console.WriteLine("06.12.2025 12:48:23");
